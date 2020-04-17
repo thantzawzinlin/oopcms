39d01964ffb64018df37f21270dd7c0712c7944d
unknown <thantzawzinlin@gmail.com>
2020-04-17 14:14:47 +0630

    Editing pages layouts

---
+++ admin/includes/side_nav.php
@@ -0,0 +1,17 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="user.php"><i class="fa fa-fw fa-bar-chart-o"></i> User</a>
+                    </li>
+                    <li>
+                        <a href="upload.php"><i class="fa fa-fw fa-table"></i> UpLoad</a>
+                    </li>
+                    <li>
+                        <a href="photo.php"><i class="fa fa-fw fa-edit"></i> Photo</a>
+                    </li>
+                    
+                </ul>
+            </div>
